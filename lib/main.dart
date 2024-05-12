6628b88e8af315bc9e0dffdd38764df25f774181
vred29 <81246734+vred29@users.noreply.github.com>
2024-05-12 19:18:14 +0300

    notifications

--- lib/main.dart
+++ lib/main.dart
@@ -18,6 +18,9 @@ void main() async {
   // notifications
   FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
       FlutterLocalNotificationsPlugin();
+  flutterLocalNotificationsPlugin.resolvePlatformSpecificImplementation<
+    AndroidFlutterLocalNotificationsPlugin>()?.requestNotificationsPermission();
+  
   // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
   const AndroidInitializationSettings initializationSettingsAndroid =
       AndroidInitializationSettings('ic_launcher');
